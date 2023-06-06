67eb1b50ec89764e3b45ed509c9cade43ae32c44
suria-raul <stadnitzki.nero.nero@gmail.com>
2023-06-06 21:58:45 +0800

    still working

--- exam.php
+++ exam.php
@@ -12,7 +12,7 @@ function scientificNotationName(int $number)
         3 => 'HUNDRED',
         4 => 'THOUSAND',
         5 => 'THOUSAND', //TEN
-        6 => 'HUNDRED THOUSAND',
+        6 => 'THOUSAND',
         7 => 'MILLION',
         8 => 'MILLION', //TEN
         9 => 'HUNDRED MILLION',
@@ -35,6 +35,96 @@ function numberNames(int $number)
         7 => 'SEVEN',
         8 => 'EIGHT',
         9 => 'NINE',
+//        10 => 'TEN',
+//        11 => 'ELEVEN',
+//        12 => 'TWELVE',
+//        13 => 'THIRTEEN',
+//        14 => 'FOURTEEN',
+//        15 => 'FIFTEEN',
+//        16 => 'SIXTEEN',
+//        17 => 'SEVENTEEN',
+//        18 => 'EIGHTEEN',
+//        19 => 'NINETEEN',
+//        20 => 'TWENTY',
+//        21 => 'TWENTY ONE',
+//        22 => 'TWENTY TWO',
+//        23 => 'TWENTY THREE',
+//        24 => 'TWENTY FOUR',
+//        25 => 'TWENTY FIVE',
+//        26 => 'TWENTY SIX',
+//        27 => 'TWENTY SEVEN',
+//        28 => 'TWENTY EIGHT',
+//        29 => 'TWENTY NINE',
+//        30 => 'THIRTY',
+//        31 => 'THIRTY ONE',
+//        32 => 'THIRTY TWO',
+//        33 => 'THIRTY THREE',
+//        34 => 'THIRTY FOUR',
+//        35 => 'THIRTY FIVE',
+//        36 => 'THIRTY SIX',
+//        37 => 'THIRTY SEVEN',
+//        38 => 'THIRTY EIGHT',
+//        39 => 'THIRTY NINE',
+//        40 => 'FORTY',
+//        41 => 'FORTY ONE',
+//        42 => 'FORTY TWO',
+//        43 => 'FORTY THREE',
+//        44 => 'FORTY FOUR',
+//        45 => 'FORTY FIVE',
+//        46 => 'FORTY SIX',
+//        47 => 'FORTY SEVEN',
+//        48 => 'FORTY EIGHT',
+//        49 => 'FORTY NINE',
+//        50 => 'FIFTY',
+//        51 => 'FIFTY ONE',
+//        52 => 'FIFTY TWO',
+//        53 => 'FIFTY THREE',
+//        54 => 'FIFTY FOUR',
+//        55 => 'FIFTY FIVE',
+//        56 => 'FIFTY SIX',
+//        57 => 'FIFTY SEVEN',
+//        58 => 'FIFTY EIGHT',
+//        59 => 'FIFTY NINE',
+//        60 => 'SIXTY',
+//        61 => 'SIXTY ONE',
+//        62 => 'SIXTY TWO',
+//        63 => 'SIXTY THREE',
+//        64 => 'SIXTY FOUR',
+//        65 => 'SIXTY FIVE',
+//        66 => 'SIXTY SIX',
+//        67 => 'SIXTY SEVEN',
+//        68 => 'SIXTY EIGHT',
+//        69 => 'SIXTY NINE',
+//        70 => 'SEVENTY',
+//        71 => 'SEVENTY ONE',
+//        72 => 'SEVENTY TWO',
+//        73 => 'SEVENTY THREE',
+//        74 => 'SEVENTY FOUR',
+//        75 => 'SEVENTY FIVE',
+//        76 => 'SEVENTY SIX',
+//        77 => 'SEVENTY SEVEN',
+//        78 => 'SEVENTY EIGHT',
+//        79 => 'SEVENTY NINE',
+//        80 => 'EIGHTY',
+//        81 => 'EIGHTY ONE',
+//        82 => 'EIGHTY TWO',
+//        83 => 'EIGHTY THREE',
+//        84 => 'EIGHTY FOUR',
+//        85 => 'EIGHTY FIVE',
+//        86 => 'EIGHTY SIX',
+//        87 => 'EIGHTY SEVEN',
+//        88 => 'EIGHTY EIGHT',
+//        89 => 'EIGHTY NINE',
+//        90 => 'NINETY',
+//        91 => 'NINETY ONE',
+//        92 => 'NINETY TWO',
+//        93 => 'NINETY THREE',
+//        94 => 'NINETY FOUR',
+//        95 => 'NINETY FIVE',
+//        96 => 'NINETY SIX',
+//        97 => 'NINETY SEVEN',
+//        98 => 'NINETY EIGHT',
+//        99 => 'NINETY NINE',
     ];
 
     if (strlen($number) == 2) {
@@ -234,7 +324,7 @@ function oneHundred(int $number)
         return 'ONE';
     }
 
-    return 'ONE HUNDRED ' . numberNames(number: $digits[1]);
+    return 'ONE HUNDRED ' . numberNames(number: $digits[1] . $digits[2]);
 }
 
 function twoHundred(int $number)
@@ -245,7 +335,7 @@ function twoHundred(int $number)
         return 'TWO';
     }
 
-    return 'TWO HUNDRED ' . numberNames(number: $digits[1]);
+    return 'TWO HUNDRED ' . numberNames(number: $digits[1] . $digits[2]);
 }
 
 function threeHundred(int $number)
@@ -256,7 +346,7 @@ function threeHundred(int $number)
         return 'THREE';
     }
 
-    return 'THREE HUNDRED ' . numberNames(number: $digits[1]);
+    return 'THREE HUNDRED ' . numberNames(number: $digits[1] . $digits[2]);
 }
 
 function fourHundred(int $number)
@@ -267,7 +357,7 @@ function fourHundred(int $number)
         return 'FOUR';
     }
 
-    return 'FOUR HUNDRED ' . numberNames(number: $digits[1]);
+    return 'FOUR HUNDRED ' . numberNames(number: $digits[1] . $digits[2]);
 }
 
 function fiveHundred(int $number)
@@ -278,7 +368,7 @@ function fiveHundred(int $number)
         return 'FIVE';
     }
 
-    return 'FIVE HUNDRED ' . numberNames(number: $digits[1]);
+    return 'FIVE HUNDRED ' . numberNames(number: $digits[1] . $digits[2]);
 }
 
 function sixHundred(int $number)
@@ -289,7 +379,7 @@ function sixHundred(int $number)
         return 'SIX';
     }
 
-    return 'SIX HUNDRED ' . numberNames(number: $digits[1]);
+    return 'SIX HUNDRED ' . numberNames(number: $digits[1] . $digits[2]);
 }
 
 function sevenHundred(int $number)
@@ -300,7 +390,7 @@ function sevenHundred(int $number)
         return 'SEVEN';
     }
 
-    return 'SEVEN HUNDRED ' . numberNames(number: $digits[1]);
+    return 'SEVEN HUNDRED ' . numberNames(number: $digits[1] . $digits[2]);
 }
 
 function eightHundred(int $number)
@@ -311,7 +401,7 @@ function eightHundred(int $number)
         return 'EIGHT';
     }
 
-    return 'EIGHT HUNDRED ' . numberNames(number: $digits[1]);
+    return 'EIGHT HUNDRED ' . numberNames(number: $digits[1] . $digits[2]);
 }
 
 function nineHundred(int $number)
@@ -322,7 +412,7 @@ function nineHundred(int $number)
         return 'NINE';
     }
 
-    return 'NINE HUNDRED ' . numberNames(number: $digits[1]);
+    return 'NINE HUNDRED ' . numberNames(number: $digits[1] . $digits[2]);
 }
 
 function cent($number)
@@ -385,4 +475,12 @@ function numberToWords($number)
     return implode(' ', $numberToWords) . cent($number[1]) . PHP_EOL;
 }
 
-print_r(numberToWords(789481.00));
+print_r(numberToWords(10002005.77));
+
+
+// 100001.01 failed (hundred thousand)
+// 1789501.25 pass
+// 789481.00 pass
+// 2156175.50 failed (5 cents)
+// 1111111.11 pass
+// 10002005.77 pass
