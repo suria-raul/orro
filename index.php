93b99cd1e0b9707a15bdad8084e819c300168d2d
suria-raul <stadnitzki.nero.nero@gmail.com>
2023-06-07 11:20:12 +0800

    removed orro.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "The Exam for orro group";
+require_once "vendor/autoload.php";
+
+$testNumbers = [
+    100001.01,
+    1789501.25,
+    789481.00,
+    2156175.50,
+    1111111.11,
+    10002005.77,
+    99999999.99, //highest number that this script can handle
+];
+
+foreach ($testNumbers as $number) {
+    echo (new \App\Controller\NumberController())->numberToWords(number: $number);
+}
