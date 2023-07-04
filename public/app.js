b4159f6a2e5f60385e0232600d3f181fc5b3eeb2
suria-raul <stadnitzki.nero.nero@gmail.com>
2023-07-04 16:52:02 +0800

    refactor

--- public/app.js
+++ public/app.js
@@ -8,7 +8,7 @@ function submitNumber() {
         let formData = new FormData(form)
 
         let request = new XMLHttpRequest()
-        request.open("POST", "../processor.php", true)
+        request.open("POST", "./processor.php", true)
         request.onreadystatechange = function() {
             if (this.readyState === 4 && this.status === 200) {
                 let result = this.responseText
